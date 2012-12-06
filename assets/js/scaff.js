f16c08c1bc52ca5ff157e040d333dad32f5339b6
Adam Cox <adamdama@hotmail.com>
2012-12-06 00:14:04 +0000

    Added distribute method for arranging elements. Panels are now elements.

--- assets/js/scaff.js
+++ assets/js/scaff.js
@@ -8,15 +8,17 @@
  */
 
 /**
- * Scaff constructor sets the root property ready for initialisation
+ * Scaff constructor sets the $root property ready for initialisation
+ * @class Scaff
  * @constructor
- * @param {Object} selector Object or String to be passed to jQuery selector to
- * be set as root property
+ * @property {jQuery} $root jQuery object of root element
+ * @property {Array} elements a collection of all root the elements that make up the scaffolding
+ * @param {Object} selector Object or String to be passed to jQuery selector to be set as $root property
  */
 var Scaff = function(selector)
 {
-	this.root = $(selector);
-	this.panels = new Array();
+	this.$root = $(selector);
+	this.elements = new Array();
 };
 
 /**
@@ -24,43 +26,103 @@ var Scaff = function(selector)
  */
 Scaff.prototype =
 {
+	/**#@+
+	  @memberOf Scaff
+	*/
 	/**
-	 * The root element of Scaff stored as a jQuery object
+	 * The $root element of Scaff stored as a jQuery object
+	 * @type jQuery
 	 */
-	root: null,
+	$root: null,
 	/**
-	 * Collection of all the panels that make up Scaff
+	 * Collection of all the elements that make up Scaff
+	 * @type Array
 	 */
-	panels: null,
+	elements: null,
 	/**
 	 * Initialisation method for Scaff.
-	 * Collects all divs on root and arranges them
+	 * Collects all divs on $root and calls distribute
+	 * @see Scaff.distribute
 	 */
 	init: function()
 	{
-		// put the first panel on the left hand side
-		var angle = -75,
-			left = 0,
-			scaff = this;
+		var scaff = this;
 
-		// loop over panels positioning them
-		$('div', this.root).each(function(index)
+		// loop over elements positioning them
+		$('div', this.$root).each(function(index)
 		{
 			var $this = $(this);
-			$this.addClass('panel ' + $this.index()).css(
+			
+			$this.addClass('elem ' + $this.index());
+			scaff.elements.push($this);
+
+		}).wrapAll($('<div id="scaffolding" />')).wrapAll('<div class="frame" />');
+		
+		this.distribute();
+		
+		//fade in scaff
+		this.$root.fadeIn(1000);
+	},
+	/**
+	 * Method to distribute the elements.
+	 * Sets next and previous element data on each element.
+	 * Sets each elements place in the scaffolding
+	 */
+	distribute: function()
+	{
+		if(this.elements.length === 0)
+			return;
+			
+		var prev,
+			first,
+			angle = 0,
+			left = 0
+			axis = ['X','Y','Z'],
+			count = 0,
+			/** 
+			 * @description Utility function for getting the next axis
+			 * @return {string} X, Y or Z */
+			getAxis = function()
+			{
+				var a = axis[count];
+				
+				count++;
+				
+				if(count >= axis.length)
+					count = 0;
+					
+				return a;
+			};
+			
+		$(this.elements).each(function()
+		{
+			// set data on each element about the next and previous element
+			var $this = $(this);
+			
+			$this.data('prev', prev);
+				
+			if(prev)
+				prev.data('next', $this);
+			
+			prev = $this;
+			
+			if(!first)
+				first = $this;
+			
+			// position each element			
+			$this.css(
 			{
 				'position': 'absolute',
 				'left': left,
-				'-webkit-transform': 'rotateY('+angle+'deg) translateZ('+$this.width()*$this.index()+'px)'
-			});
-
-			scaff.panels.push($this);
+				'-webkit-transform': 'rotate'+getAxis()+'('+angle+'deg) translate'+getAxis()+'(100px)',
+				'-webkit-perspective': left+'px'
+			})
 
 			left += $this.width();
-			angle += 90
-
-		}).wrapAll($('<div id="scaffolding" />'));
+			angle += 45 + Math.round(Math.random() * 35);
+		});
 	}
+	/**#@-*/
 };
 
 // /**
